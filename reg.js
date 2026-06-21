34d6c3adcc45c896965f68e1174ce252b0e62cc2
shodiyor986 <156547943+shodiyor986@users.noreply.github.com>
2026-06-20 20:31:56 -0700

    Update reg.js with refined logic

--- reg.js
+++ reg.js
@@ -4,7 +4,7 @@
 
   // Utility: generate UUID v4
   const genUUID = () => {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c/4).toString(16)
     );
   };
